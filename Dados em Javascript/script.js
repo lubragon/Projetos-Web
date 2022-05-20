9adfc2069845b51a141a28639a3bc05bfab2a4a8
Lucas Bragança Gonçalves <100163176+lubragon@users.noreply.github.com>
2022-05-20 20:44:48 -0300

    esboço

---
+++ Dados em Javascript/script.js
@@ -0,0 +1,27 @@
+async function roll() {
+
+  
+    
+    var d20 = Math.floor(Math.random()*20)
+    var d12 = Math.floor(Math.random()*12)
+    var d10 = Math.floor(Math.random()*10)
+    var d8 = Math.floor(Math.random()*8)
+    var d6 = Math.floor(Math.random()*6)
+    var d4 = Math.floor(Math.random()*4)
+
+    var rol = document.querySelector('input[name="dice"]:checked').value
+  
+
+
+}
+
+
+
+document.getElementById("res").innerHTML = rol
+   
+
+
+
+
+
+var rol = document.getElementById('input[name="dic"]:checked')
