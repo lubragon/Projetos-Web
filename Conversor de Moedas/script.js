f8391a2cf6abbc39bcc4f3c34bcd93c620d34171
Lucas Bragança Gonçalves <100163176+lubragon@users.noreply.github.com>
2022-05-19 22:25:48 -0300

    Update script.js

--- Conversor de Moedas/script.js
+++ Conversor de Moedas/script.js
@@ -1,1 +1,16 @@
-a
+async function converter() {
+    var quantia = document.getElementById("quantia").value
+
+    var min = document.querySelector('input[name="in"]:checked').value
+    var mout = document.querySelector('input[name="out"]:checked').value
+
+    var url =
+    "https://api.exchangerate.host/convert?from=" + min + "&to=" + mout
+
+    var response = await fetch(url)
+    var data = await response.json()
+
+    var valorConvertido = quantia * data.info.rate
+    
+    document.getElementById("res").innerHTML = valorConvertido.toFixed(2)
+}
