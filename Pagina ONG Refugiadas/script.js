c6a8539d7dbee00114d8df85ccad89b676f63628
Lucas Bragança Gonçalves <100163176+lubragon@users.noreply.github.com>
2022-06-01 21:05:50 -0300

    Update script.js

--- Pagina ONG Refugiadas/script.js
+++ Pagina ONG Refugiadas/script.js
@@ -1,5 +1,53 @@
-function sub() {
-var g = document.getElementsByTagName("ab") = gre
-g.innerHTML = grey
 
+var wa = document.getElementById("grey")
+
+
+function sub() {
+    $("#grey")[0].style["display"]= "flex";
+    $("#but")[0].style["background-color"]= "#005BBC";
+
+
+    console.log("a")
+}
+
+    
+function clos() {
+    $("#grey")[0].style["display"]= "none";
+    $("#but")[0].style["background-color"]= "#005BBC";
+    console.log("a")
+}
+
+
+
+function fotoum() {
+    $("#sala")[0].style["zIndex"]= "3";
+    $("#quarto")[0].style["zIndex"]= "2";
+    $("#cozinha")[0].style["zIndex"]= "2";
+
+    $("#but1")[0].style["background-color"]= "#005BBC";
+    $("#but2")[0].style["background-color"]= "white";
+    $("#but3")[0].style["background-color"]= "white";
+    console.log("a")
+}
+
+function fotodois() {
+    $("#sala")[0].style["zIndex"]= "2";
+    $("#cozinha")[0].style["zIndex"]= "3";
+    $("#quarto")[0].style["zIndex"]= "2";
+
+    $("#but1")[0].style["background-color"]= "white";
+    $("#but2")[0].style["background-color"]= "#005BBC";
+    $("#but3")[0].style["background-color"]= "white";
+    console.log("a")
+}
+
+function fototres() {
+    $("#sala")[0].style["zIndex"]= "2";
+    $("#cozinha")[0].style["zIndex"]= "2";
+    $("#quarto")[0].style["zIndex"]= "3";
+
+    $("#but1")[0].style["background-color"]= "white";
+    $("#but2")[0].style["background-color"]= "white";
+    $("#but3")[0].style["background-color"]= "#005BBC";
+    console.log("a")
 }
